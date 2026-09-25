0997b9baee07b4aeb1e2af9d705236cdd7f287d1
Phillip-T2 <phillip.terblanche@gmail.com>
2026-09-25 10:41:14 +0200

    Move Quartus files into Practicals folder

---
+++ Practicals/MegaFunctions/SourcesAndProbes/SourcesAndProbes_bb.v
@@ -0,0 +1,8 @@
+
+module SourcesAndProbes (
+	source,
+	probe);	
+
+	output	[9:0]	source;
+	input	[9:0]	probe;
+endmodule
---
+++ Practicals/MegaFunctions/SourcesAndProbes/SourcesAndProbes_inst.vhd
@@ -0,0 +1,13 @@
+	component SourcesAndProbes is
+		port (
+			source : out std_logic_vector(9 downto 0);                    -- source
+			probe  : in  std_logic_vector(9 downto 0) := (others => 'X')  -- probe
+		);
+	end component SourcesAndProbes;
+
+	u0 : component SourcesAndProbes
+		port map (
+			source => CONNECTED_TO_source, -- sources.source
+			probe  => CONNECTED_TO_probe   --  probes.probe
+		);
+
